3a0f05aa79dad9312679aef79370fe3baae19e0f
Rafael Salgues <rafaelsalgues0238@gmail.com>
2025-09-25 13:29:25 -0300

    implementando registerBook como metodo de clase

--- main.py
+++ main.py
@@ -11,30 +11,6 @@ loan = []
 def save_book_db(book_data):
     books_db.append(book_data)
 
-def register_new_book():
-    
-    max_id = 0
-    for livro in books_db:
-        current_id = livro["idBook"]
-        if current_id > max_id:
-            max_id = current_id
-    new_id = max_id + 1
-    title = input("Insert title: ")
-    author = input("Insert Author: ")
-    genre = input("Insert genre: ")
-    publish_date = input("Insert publish date (AAAA-MM-DD): ")
-
-    new_book = Book(
-        idBook=new_id, 
-        title=title, 
-        author=author, 
-        genre=genre, 
-        publish_date=publish_date
-    )
-
-    new_book.registerBook()
-    
-    print(f"Book '{title}'(ID: {new_id}) registered.")
 
 class Book:
     def __init__(self, idBook, title, author, genre, publish_date):
@@ -43,6 +19,31 @@ class Book:
         self.author = author
         self.genre = genre
         self.publish_date = publish_date
+    
+    @classmethod
+    def registerBook(cls):
+    
+        max_id = 0
+        for livro in books_db:
+            current_id = livro["idBook"]
+            if current_id > max_id:
+                max_id = current_id
+        new_id = max_id + 1
+        title = input("Insert title: ")
+        author = input("Insert Author: ")
+        genre = input("Insert genre: ")
+        publish_date = input("Insert publish date (AAAA-MM-DD): ")
+
+        print(f"Book '{title}'(ID: {new_id}) registered.")
+
+        new_book = cls(
+            idBook=new_id, 
+            title=title, 
+            author=author, 
+            genre=genre, 
+            publish_date=publish_date
+            )
+        save_book_db(new_book.to_dict())
 
     def to_dict(self):
         return {
@@ -53,8 +54,5 @@ class Book:
             "publish_date": self.publish_date
         }
 
-    def registerBook(self):
 
-        book_data = self.to_dict()
-        save_book_db(book_data)
-
+Book.registerBook()
